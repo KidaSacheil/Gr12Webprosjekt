d1774475b33a05c3b6a124abc9d9a94550ed1404
KidaSacheil <Kassandra994@live.no>
2013-11-16 23:24:26 +0100

    Sluttrapport
    Arbeid startet med sluttrapport. arbeidet med oppsett

---
+++ Sluttrapport/sluttrapport.php
@@ -0,0 +1,59 @@
+<?php
+$page= $_GET["page"];
+
+?><!DOCTYPE HTML>
+<html>
+<head>
+	<title>Sluttrapport - Gruppe 12 </title>
+	<meta charset="UTF-8">
+	<link rel="stylesheet" type="text/css" href="./css/sluttrapportstyle.css">
+</head>
+
+<body>
+	<div id="wrapper">
+
+		<nav>
+				<h1><a href="forside.html">Gruppe 12</a></h1>
+
+				<h2>Gruppemedlemmer:</h2>
+			<ul>
+				<li><a href="http://student.cs.hioa.no/~s198541/webprosjekt2/index.php?page=0">Helene Juterud</a></li>
+				<li>Steinarr Sommerset</li>
+				<li>Patrik Mørkeseth</li>
+				<li>Sigurd Hølleland</li>
+			</ul>
+
+				<h2>Sluttrapport:</h2>
+			<ul>
+				<li>Sluttrapport</li>
+				
+			</ul>
+
+
+				<h2>Dokumenter:</h2>
+			<ul>
+				<li>Forprosjektrapport</li>
+				<li>Kravspec</li>
+				<li>Gruppeavtale</li>
+				<li>Møterapporter</li>
+			</ul>
+
+		</nav>
+
+
+	<section>
+		<?php
+			switch($page){
+
+				default
+				include("./forside.html");
+				break;
+			}
+		?>
+	</section>
+
+	</div>
+
+
+</body>
+</html>
