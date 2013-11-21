2973a03cb8185f90c1d38e95d65f43bfd40619d8
sommerz <sommerz@gmail.com>
2013-11-21 17:40:01 +0100

    Meir styling av test

--- Nettstedet/js/testen.js
+++ Nettstedet/js/testen.js
@@ -1,3 +1,4 @@
+// Kategorier sortert i variabler
 var petanque=0;
 var gokart=0;
 var orientering=0;
@@ -8,6 +9,37 @@ var bridge=0;
 var flyspotting=0;
 var spmTeller=5;
 var sportEllerHobby;
+var urlen;
+
+	// Første side
+	function velkommen(){
+
+		document.getElementById("spmSvarA").style.display="none";
+		document.getElementById("spmSvarB").style.display="none";
+		document.getElementById("resID").style.display="none";
+		document.getElementById("resOversikt").style.display="none";
+		document.getElementById("velkommen").style.display="block";
+
+		var strengC = document.getElementById("testOverskrift").innerHTML; 
+		var endraTekstA = strengC.replace("x", "FRITIDSMASKIN");
+		document.getElementById("testOverskrift").innerHTML=endraTekstA;
+	}
+
+	function startTest(){
+
+	document.getElementById("spmSvarA").style.display="block";
+		document.getElementById("spmSvarB").style.display="block";
+		document.getElementById("resID").style.display="none";
+		document.getElementById("resOversikt").style.display="none";
+		document.getElementById("velkommen").style.display="none";
+
+		var strengC = document.getElementById("testOverskrift").innerHTML; 
+		var endraTekstA = strengC.replace("FRITIDSMASKIN", "TEST: SPM. 1");
+		document.getElementById("testOverskrift").innerHTML=endraTekstA;
+
+		document.getElementById("h1").style.color="black";
+		document.getElementById("h1").style.backgroundColor="yellow";
+	}
 
 function svarA(){ // Klikk på knapp A (til venstre)
 
@@ -34,7 +66,7 @@ function svarA(){ // Klikk på knapp A (til venstre)
 
 			else if (spmTeller==8 && sportEllerHobby=="sport") {
 				spmPoeng(2,2,1,3,0,0,0,0);
-				spmHTML("Konsentrasjon", "Lavt budsjett", "Kondisjon", "Høyt budsjett", " 4", " 5");
+				spmHTML("Konsentrasjon", "Lite budsjett", "Kondisjon", "Stort budsjett", " 4", " 5");
 				spmTeller++;
 			}
 			
@@ -55,7 +87,7 @@ function svarA(){ // Klikk på knapp A (til venstre)
 
 			else if (spmTeller==8 && sportEllerHobby=="hobby") {
 				spmPoeng(0,0,0,0,2,2,1,1);
-				spmHTML("Kos", "Kreativ", "Spenning", "Tålmodig", " 4", " 5");
+				spmHTML("Kos", "Kreativ", "Spenning", "Rolig", " 4", " 5");
 				spmTeller++;
 			}
 			else if (spmTeller==9 && sportEllerHobby=="hobby") {
@@ -90,7 +122,7 @@ function svarA(){ // Klikk på knapp A (til venstre)
 				
 			else if (spmTeller==8 && sportEllerHobby=="sport") {
 				spmPoeng(0,1,3,0,0,0,0,0);
-				spmHTML("Konsentrasjon", "Lavt budsjett", "Kondisjon", "Høyt budsjett", " 4", " 5"); // PASS PÅ SPACE ETTER 1 FOR AT DET SKAL FUNKE, MOGELEG Å LESE inn SPACE-EN i staden? SPØR FAGFOLK! forrigeTekstInnA, nyTekstInnA, forrigeTekstInnB, nyTekstInnB, forrigeOverskrift, nyOverskrift // <-- HAR PRØVD Å ENDRE DENNE, SJÅ LENGER NED			spmTeller++;
+				spmHTML("Konsentrasjon", "Lite budsjett", "Kondisjon", "Stort budsjett", " 4", " 5"); // PASS PÅ SPACE ETTER 1 FOR AT DET SKAL FUNKE, MOGELEG Å LESE inn SPACE-EN i staden? SPØR FAGFOLK! forrigeTekstInnA, nyTekstInnA, forrigeTekstInnB, nyTekstInnB, forrigeOverskrift, nyOverskrift // <-- HAR PRØVD Å ENDRE DENNE, SJÅ LENGER NED			spmTeller++;
 				spmTeller++;
 			}
 			
@@ -111,7 +143,7 @@ function svarA(){ // Klikk på knapp A (til venstre)
 
 			else if (spmTeller==8 && sportEllerHobby=="hobby") {
 				spmPoeng(0,0,0,0,0,0,1,2);
-				spmHTML("Kos", "Kreativ", "Spenning", "Tålmodig", " 4", " 5");
+				spmHTML("Kos", "Kreativ", "Spenning", "Rolig", " 4", " 5");
 				spmTeller++;
 			}
 
@@ -154,28 +186,28 @@ function spmHTML(forrigeTekstInnA, nyTekstInnA, forrigeTekstInnB, nyTekstInnB, f
 function aktivitetar(){
 
 var aktivitetar=[];
-	aktivitetar[0]={navn:"petanque",poeng:petanque,url:""}
-	aktivitetar[1]={navn:"gokart",poeng:gokart,url:""}
-	aktivitetar[2]={navn:"orientering",poeng:orientering,url:""}
-	aktivitetar[3]={navn:"bueskytting",poeng:bueskytting,url:""}
-	aktivitetar[4]={navn:"strikking",poeng:strikking,url:""}
-	aktivitetar[5]={navn:"matfoto",poeng:matfoto,url:""}
-	aktivitetar[6]={navn:"bridge",poeng:bridge,url:""}
-	aktivitetar[7]={navn:"flyspotting",poeng:flyspotting,url:""}
+	aktivitetar[0]={navn:"petanque",poeng:petanque,url:"petanque.html"}
+	aktivitetar[1]={navn:"gokart",poeng:gokart,url:"gokart.html"}
+	aktivitetar[2]={navn:"orientering",poeng:orientering,url:"orientering.html"}
+	aktivitetar[3]={navn:"bueskytting",poeng:bueskytting,url:"bueskyting.html"}
+	aktivitetar[4]={navn:"strikking",poeng:strikking,url:"strikking.html"}
+	aktivitetar[5]={navn:"matfoto",poeng:matfoto,url:"matfot.html"}
+	aktivitetar[6]={navn:"bridge",poeng:bridge,url:"bridge.html"}
+	aktivitetar[7]={navn:"flyspotting",poeng:flyspotting,url:"flyspotting.html"}
 
 		// Sortering etter høgste sum:
 		aktivitetar.sort(function(en, to){
 		return to.poeng-en.poeng;
 		})
 
-	return [aktivitetar[0].navn,aktivitetar[1].navn,aktivitetar[2].navn,aktivitetar[3].navn,aktivitetar[4].navn,aktivitetar[5].navn,aktivitetar[6].navn,aktivitetar[7].navn];
+	return [aktivitetar[0].navn,aktivitetar[1].navn,aktivitetar[2].navn,aktivitetar[3].navn,aktivitetar[4].navn,aktivitetar[5].navn,aktivitetar[6].navn,aktivitetar[7].navn,aktivitetar[0].url];
 }
 
 // Printing av resultat
-	function resultatPrint(){
+function resultatPrint(){
 
 	var aktArray = aktivitetar();
-	var akt1st = aktArray[0];
+	var akt1st = aktArray[0]; akt1st = akt1st.toUpperCase(); 
 	var akt2nd = aktArray[1];
 	var akt3rd = aktArray[2];
 	var akt4th = aktArray[3];
@@ -183,6 +215,9 @@ var aktivitetar=[];
 	var akt6th = aktArray[5];
 	var akt7th = aktArray[6];
 	var akt8th = aktArray[7];
+	var akt1url = aktArray[8];
+
+	urlen = akt1url; // Her blir adressa til testvinnaren sendt ut til den globale variabelen url, som igjen vert kalla i sluttfunksjonen.
 
 		document.getElementById("spmSvarA").style.display="none";
 		document.getElementById("spmSvarB").style.display="none";
@@ -190,14 +225,36 @@ var aktivitetar=[];
 		document.getElementById("resOversikt").style.display="block";
 
 		var strengC = document.getElementById("testOverskrift").innerHTML; 
-		var endraTekstA = strengC.replace("Test: Spørsmål 5", "Resultat");
+		var endraTekstA = strengC.replace("TEST: SPM. 5", "TESTRESULTAT:");
 		document.getElementById("testOverskrift").innerHTML=endraTekstA;
 
+		//egen id for berre akt1st så den kan skifte farge dynamisk
+		var strengD2 = document.getElementById("akt1stID").innerHTML; 
+		var endraTekstD2 = strengD2.replace("",akt1st);
+		document.getElementById("akt1stID").innerHTML=endraTekstD2;
+
+		if (sportEllerHobby=="sport"){
+			document.getElementById("akt1stID").style.color="red";
+		}
+		
+		else if (sportEllerHobby=="hobby"){
+			document.getElementById("akt1stID").style.color="blue";
+		}	
+
 		var strengD = document.getElementById("resID").innerHTML; 
-		var endraTekstD = strengD.replace("","Aktiviteten som passer best for deg, er "+akt1st+".");
+		var endraTekstD = strengD.replace("","Aktiviteten som passer best for deg: ");
 		document.getElementById("resID").innerHTML=endraTekstD;
 
 		var strengE = document.getElementById("resOversikt").innerHTML; 
-		var endraTekstE = strengE.replace("","Resultat:<br /><br />"+akt1st+"<br />"+akt2nd+"<br />"+akt3rd+"<br />"+akt4th+"<br />"+akt5th+"<br />"+akt6th+"<br />"+akt7th+"<br />"+akt8th);
+		var endraTekstE = strengE.replace("","Resultatoversikt:<br /><br />"+akt1st+"<br />"+akt2nd+"<br />"+akt3rd+"<br />"+akt4th+"<br />"+akt5th+"<br />"+akt6th+"<br />"+akt7th+"<br />"+akt8th);
 		document.getElementById("resOversikt").innerHTML=endraTekstE;
+
+		document.getElementById("h1").style.color="yellow";
+		document.getElementById("h1").style.backgroundColor="black";
+	
+	return akt1url;
 	}
+
+function gotoAkt(){
+	window.location.href = urlen;
+}
