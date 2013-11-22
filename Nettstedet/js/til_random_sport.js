1c8795e35b7a3219c43077baf7354ad5dcabe0a7
sommerz <sommerz@gmail.com>
2013-11-22 14:55:01 +0100

    sport hover

--- Nettstedet/js/til_random_sport.js
+++ Nettstedet/js/til_random_sport.js
@@ -1,24 +1,6 @@
 var bilder;
 //tilfeldigHobby(); // funksjonen kjører automatisk når skriptet blir lasta inn i HTML-fila
 
-//denne går bare til random side på GIF-en på forsida
-function tilfeldigAktivitet(){
-var tilfeldigTall1 = Math.floor((Math.random()*8)); // Tilfeldig tall mellom 0 og 7, runda ned til nærmeste heltall
-var aktivitetar=[];
-	aktivitetar[0]={navn:"petanque",url:"petanque.html"}
-	aktivitetar[1]={navn:"gokart",url:"gokart.html"}
-	aktivitetar[2]={navn:"orientering",url:"orientering.html"}
-	aktivitetar[3]={navn:"bueskytting",url:"bueskytting.html"}
-	aktivitetar[4]={navn:"strikking",url:"strikking.html"}
-	aktivitetar[5]={navn:"matfoto",url:"matfotografi.html"}
-	aktivitetar[6]={navn:"bridge",url:"bridge.html"}
-	aktivitetar[7]={navn:"spotting",url:"planespotting.html"}
-
-alert ("No blir du heilt tilfeldig sendt til sida for "+aktivitetar[tilfeldigTall1].navn);
-window.location.href = aktivitetar[tilfeldigTall1].url;
-}
-//random GIF-funksjon ferdig
-
 //denne bestemmer hvilket random bilde som blir vist på Sport-siden, og bildeverdien er knyttet til spesifikk side
 function tilfeldigSport(){
 var tilfeldigTallS = Math.floor((Math.random()*4)); // Tilfeldig tall mellom 0 og 3, runda ned til nærmeste heltall
@@ -37,8 +19,11 @@ var tilfeldigBildeArrayS = tilfeldigSport();
 var tilfeldigBildeArrayUrlS = tilfeldigBildeArrayS[0];
 var tilfeldigBildeArrayNamnS = tilfeldigBildeArrayS[1];
 
+		var gjemt = document.getElementById("gjemt").innerHTML; 
+		var gjemtEndra = gjemt.replace("ph", tilfeldigBildeArrayNamnS);
+		document.getElementById("gjemt").innerHTML=gjemtEndra;
+
 function tilfeldigSportLink(){
 	
-	alert ("No blir du derimot sendt til sida for den tilfeldige aktiviteten du ser bilde av (når riktig bilde er på plass), altså "+tilfeldigBildeArrayNamnS);
 	window.location.href = tilfeldigBildeArrayUrlS; // return-verdien, altså url-en
 }
